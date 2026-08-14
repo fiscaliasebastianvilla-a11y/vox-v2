a30b3a4b1597a38f7acd8af849ea9b66f5e61b52
fiscaliasebastianvilla-a11y <fiscalia.sebastian.villa@gmail.com>
2026-08-14 20:25:24 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE='vox-v2-static-v1';
+const ASSETS=['./','index.html','manifest.webmanifest','vox-icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
